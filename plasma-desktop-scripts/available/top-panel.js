b7b803bbb0805d444faa0319f067eb718f2da97d
laksith19 <admin@laksith.dev>
2025-03-15 22:36:14 -0700

    add paper top panel

--- plasma-desktop-scripts/available/top-panel.js
+++ plasma-desktop-scripts/available/top-panel.js
@@ -10,12 +10,17 @@ pager.writeConfig("showOnlyCurrentScreen", false)
 var spacer = newPanel.addWidget("org.kde.plasma.panelspacer")
 spacer.expandable = true
 
+var paper = newPanel.addWidget("com.github.zren.commandoutput")
+paper.writeConfig("command", "paper-genmon")
+paper.writeConfig("fontSize", "10")
+
 var clock = newPanel.addWidget("org.kde.plasma.digitalclock")
 clock.currentConfigGroup = ["Appearance"]
 clock.writeConfig("showDate", true)
 
 
 var logout = newPanel.addWidget("org.kde.plasma.lock_logout")
+
 
 
 
